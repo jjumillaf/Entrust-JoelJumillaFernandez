87385e6e799b8b0fde03834a92df13782dde761d
jjumillaf <92712263+jjumillaf@users.noreply.github.com>
2024-06-28 19:36:30 +0200

    bash testing added

---
+++ testing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Compile the C++ files if not already compiled
+# (Uncomment these lines if needed)
+g++ -o app app.cpp
+g++ -o tested test/test.cpp
+
+# Execute the app passing the three text files as arguments
+./app test/empty.txt test/empty_output.txt 80 25
+./app test/longer_maxchars.txt test/longer_maxchars_output.txt 80 25
+./app test/max_lenght_word.txt test/max_lenght_word_output.txt 80 25
+
+# Check if app executed successfully
+if [ $? -eq 0 ]; then
+    echo "app executed successfully, now running test on output.txt"
+    # Execute the test code on the output file
+    ./tested test/empty_output.txt 80 25
+    ./tested test/longer_maxchars_output.txt 80 25
+    ./tested test/max_lenght_word_output.txt 80 25
+else
+    echo "app execution failed"
+fi
+
